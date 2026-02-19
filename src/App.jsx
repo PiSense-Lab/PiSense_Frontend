1960c650b875a481c4c26b6ce59538e35e4439d6
Nate Stream <nathanstream203@gmail.com>
2026-02-19 13:38:59 -0600

    Vite + React + Tailwind Setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-3xl text-center">Welcome to PiSense!</h1>
+    </div>
+    </>
+  )
+}
+
+export default App

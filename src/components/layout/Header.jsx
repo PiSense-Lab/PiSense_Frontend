5ef50e0832f35c036107b34b55481ef7076deb65
Nate Stream <nathanstream203@gmail.com>
2026-03-04 12:12:22 -0600

    File Management

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,13 +1,14 @@
-import React, { useState } from "react";
-import ToggleDarkMode from "../ui/Toggle";
+import React from "react";
+import useDarkMode from "../../hooks/useDarkMode";
 
 // ICONS //
 import { LuBell, LuSearch, LuUser } from "react-icons/lu";
-import DropDownProfile from "../ui/DropDownProfile";
+import { IoSunny } from "react-icons/io5";
+import { HiMiniMoon } from "react-icons/hi2";
 // ICONS //
 
 const Header = () => {
-  const [profileDropDown, setProfileDropDown] = useState(false);
+  const { toggleDarkMode, isDarkMode } = useDarkMode();
 
   return (
     <>
@@ -21,7 +22,25 @@ const Header = () => {
           />
         </div>
         <div className="flex items-center space-x-3">
-          <ToggleDarkMode />
+          <button
+            onClick={toggleDarkMode}
+            className={`w-14 h-full flex items-center rounded-full p-1 ${
+              isDarkMode ? "bg-pitch" : "bg-gray-200"
+            }`}
+          >
+            <div
+              className={`flex justify-center items-center bg-white dark:bg-gray-300 w-5 h-5 rounded-full shadow-md transform transition-transform duration-300 ${
+                isDarkMode ? "translate-x-7" : "translate-x-0"
+              }`}
+            >
+              <span className="text-yellow-500 flex dark:hidden ">
+                {IoSunny()}
+              </span>
+              <span className="text-pitch hidden dark:flex dark:text-sky">
+                {HiMiniMoon()}
+              </span>
+            </div>
+          </button>
           <button className="relative text-2xl p-2 items-center justify-center rounded-full hover:bg-gray-200 dark:hover:bg-pitch">
             <LuBell
               size={25}
@@ -31,15 +50,11 @@ const Header = () => {
               3
             </span>
           </button>
-          <button
-            className="p-2 flex items-center justify-center rounded-full text-gray-600 hover:bg-gray-200 dark:text-gray-300 dark:hover:bg-pitch"
-            onClick={() => setProfileDropDown(!profileDropDown)}
-          >
+          <button className="p-2 flex items-center justify-center rounded-full text-gray-600 hover:bg-gray-200 dark:text-gray-300 dark:hover:bg-pitch">
             <LuUser size={25} />
           </button>
         </div>
       </div>
-      {profileDropDown && <DropDownProfile />}
     </>
   );
 };

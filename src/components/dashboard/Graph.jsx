45a9af2a711e941a5cc596c3f07ad62f9ffa1673
Amber Kuhn <i.am.fastk@gmail.com>
2026-04-03 11:01:16 -0500

    getting charts working

--- src/components/dashboard/Graph.jsx
+++ src/components/dashboard/Graph.jsx
@@ -1,11 +1,11 @@
 import React from "react";
+import IndexLineChart from "./Charts";
 
 const Graph = (data) => {
   if (!data) return <div>No data yet</div>;
   return (
     <div className="flex flex-col rounded-md px-8 py-4 h-full bg-white dark:bg-midnight">
-      <h1 className="text-lg font-semibold"> Graph </h1>
-      <div className="text-s"> Text </div>
+      <IndexLineChart/>
     </div>
   );
 };

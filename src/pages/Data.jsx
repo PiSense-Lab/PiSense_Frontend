f55e81e94279b5484b7123ce9740c1ac0cecee41
Amber Kuhn <i.am.fastk@gmail.com>
2026-04-18 20:53:15 -0500

    comments gone

--- src/pages/Data.jsx
+++ src/pages/Data.jsx
@@ -29,8 +29,6 @@ const Data = () => {
     fetchDatasets();
   }, []);
 
-  // get data from the backend api from projectid and put the basic table data in the datasetsmeta
-
 
   const handleViewDataset = async (dataset) => {
     if (dataset.mode === "edit") {
